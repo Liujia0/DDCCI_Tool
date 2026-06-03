aa0b335bc9651f855d132cce6765b9ac89bddeac
liujiahao8801 <liujiahao8801@cvte.com>
2026-06-03 13:30:25 +0800

    Add single-file packaging

--- resource.h
+++ resource.h
@@ -2,3 +2,10 @@
 
 #define IDI_APP_ICON    101
 #define IDR_VERSION_INFO 1
+
+// Embedded web resources (custom resource type "WEBRES")
+#define IDR_WEB_INDEX_HTML  201
+#define IDR_WEB_STYLE_CSS   202
+#define IDR_WEB_APP_JS      203
+#define IDR_WEB_MCCS_JS     204
+#define IDR_WEB_LOGO_SVG    205
